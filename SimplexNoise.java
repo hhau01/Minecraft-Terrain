33c88a48a33f43fca07bf16927246b555a0210b1
henry <henryau01@gmail.com>
2015-11-19 19:33:40 -0800

    Added package location
    because we are dumb

--- SimplexNoise.java
+++ SimplexNoise.java
@@ -1,3 +1,5 @@
+package minecraft;
+
 import java.util.Random;
 
 public class SimplexNoise {
--- SimplexNoise_octave.java
+++ SimplexNoise_octave.java
@@ -1,3 +1,5 @@
+package minecraft;
+
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
